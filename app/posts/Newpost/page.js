bddaea455fe8e8cb1aa0b096a106f9ac1b8c18ac
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2024-01-26 14:20:06 +0530

    default state

--- app/posts/Newpost/page.js
+++
@@ -1,9 +0,0 @@
-
-// localhost:3000/posts/Newpost
-
-const page = () => {
-  return (
-    <h1>New Posts Yo</h1>
-  )
-}
-export default page
--- app/posts/page.js
+++
@@ -1,9 +0,0 @@
-
-// localhost:3000/posts/
-
-const page = () => {
-  return (
-    <h1>Posts</h1>
-  )
-}
-export default page
