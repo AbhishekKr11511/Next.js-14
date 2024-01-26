251b138aecf124e97409b3b15b9a38991d8253b4
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2024-01-26 14:15:31 +0530

    fourth commit

---
+++ app/api/users/routes.js
@@ -0,0 +1,11 @@
+// localhost:3000/api/users
+
+export async function GET(request){
+    // Handle GET requests for /api/users
+    // Retrieve users from the database or any data source
+    const users = [
+        {id : 1, name: "John"},
+        {id : 2, name: "Bob"},
+        {id : 3, name: "Dave"}
+    ]
+}
