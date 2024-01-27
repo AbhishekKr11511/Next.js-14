3ecec3a73483ae0daa84b399944f8e897615eb64
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2024-01-27 12:36:24 +0530

    Added hero section

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,19 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-col flex-center">
+        <h1 className="head_text text-center">
+            Discover & Share
+            <br className="max-md:hidden"/>
+            <span className="orange_gradient text-center">AI-Powered Prompts</span>
+            <p className="desc text-center">
+                Promptopia is an open-source AI prompting tool for modern world to discover, createand share creative prompts.
+            </p>
+
+            {/*Feed */}
+            
+        </h1>
+    </section>
   )
 }
 export default Home
