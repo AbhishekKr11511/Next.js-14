cd11ab519e32c770fc61394d2a61e8c3fa59caf5
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2024-01-28 15:55:43 +0530

    Lets go

--- components/Form.jsx
+++ components/Form.jsx
@@ -26,11 +26,24 @@ const Form = ({type,post,setPost,submitting,handleSubmit}) => {
           <input 
             value={post.tag}
             onChange={(e)=>setPost({...post, tag: e.target.value})}
-            placeholder='Add Tags to your prompt'
+            placeholder='#tag'
             required
-            className='form_textarea'
+            className='form_input'
             />
         </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href={'/'} className='text-gray-500 text-sm'>
+            Cancel
+          </Link>
+
+          <button type='submit'
+            disabled={submitting}
+            className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'
+          >
+            {submitting? `${type}...`:type}
+          </button>
+        </div>
       </form>
     </section>
   )
