68557cb77f6c6d5f86c31328670016785b9a7a52
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2024-01-29 13:03:52 +0530

    Profile page is initiated

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,31 @@
-const profile = () => {
+"use client"
+
+import {useState, useEffect} from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+import Profile from '@components/Profile'
+
+
+
+const Profile = () => {
+
+  const handleEdit = () => {
+
+  }
+
+  const handleDelete = () => {
+
+  }
+  
   return (
-    <div>profile</div>
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data = {[]}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   )
 }
-export default profile
+export default Profile
